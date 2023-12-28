f3dfdf8d424a369e3f563d81dc65711a5284ffd1
LIAGM <louis19950117@yahoo.com.tw>
2023-12-28 09:34:10 -0700

    modify scripts

---
+++ scripts/run_association_stage_training.sh
@@ -0,0 +1,19 @@
+# export BASICSR_JIT=True
+export CXX=g++
+
+conf_name='Association_stage'
+
+ROOT_PATH='/ssd1/yuju/DAEFR/experiments/' # The path for saving model and logs
+
+# gpus='0,1,2,3'
+gpus='0,'
+
+#P: pretrain SL: soft learning
+node_n=1
+
+python -u main_for_association.py \
+--root-path $ROOT_PATH \
+--base 'configs/'$conf_name'.yaml' \
+-t True \
+--gpus $gpus \
+--num-nodes $node_n \
